a4edca5d970bc723387252c13ed2b3aaa360c555
incognito0007 <ankit.22010787@viit.ac.in>
2024-06-19 11:18:36 +0530

    Work on contact us form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_9xhcg2g', 'template_ewu9ubj', form.current, 'peHU74jEsuT68uwgu')
       .then((result) => {
         setOpen(true);
         form.current.reset();
